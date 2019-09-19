4bff7d99d5eef81dc9cb465c685be3a9fbfdf936
Martin Slemr <mslemr@redhat.com>
2019-09-19 15:45:20 +0200

    sources patch request added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::API
+  SOCKET_PATH = "/tmp/receptor.sock".freeze
+  DELIM = "\x1b[K".freeze
+
+  protected
+
+  def send_directive(directive, recipient, payload, socket_path = SOCKET_PATH)
+    socket = UNIXSocket.new(socket_path)
+    socket.send("#{recipient}\n#{directive}\n#{payload}" + DELIM, 0)
+    response = ''
+    loop do
+      received = socket.recv(1024)
+      done = received.include?(DELIM)
+      received.sub!(DELIM, '') if done
+
+      response += received
+      break if done
+    end
+    response
+  ensure
+    socket.close
+  end
+
+  def identity_headers(tenant)
+    {
+      "x-rh-identity" => Base64.strict_encode64(
+        JSON.dump({"identity" => {"account_number" => tenant}})
+      )
+    }
+  end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,12 @@
 require 'socket'
 
 class OrdersController < ApplicationController
-  SOCKET_PATH = "/tmp/receptor.sock".freeze
-  DELIM = "\x1b[K".freeze
-
+  # GET
   def index
-    target_receptor = "node-a" # This name has to be in sources db
     payload = {
       'method'  => 'GET',
       'url'     => "http://localhost:3002/api/sources/v1.0/sources",
-      'headers' => identity_headers('1460290')
+      'headers' => identity_headers(external_tenant)
     }
     response = send_directive("receptor_http:execute",
                               target_receptor,
@@ -17,33 +14,35 @@ class OrdersController < ApplicationController
 
     hash = JSON.parse(response)
 
-    render :plain => hash['raw_payload']['body']
+    render :json => hash.dig('raw_payload', 'body'), :status => hash.dig('raw_payload', 'status')
+  end
+
+  # GET
+  def source_update
+    payload = {
+      'method'  => 'PATCH',
+      'url'     => "http://localhost:3002/api/sources/v1.0/sources/2",
+      'data'    => {'name' => 'Openshift Changed'}.to_json,
+      'headers' => identity_headers(external_tenant)
+    }
+
+    response = send_directive("receptor_http:execute",
+                              target_receptor,
+                              payload.to_json)
+
+    hash = JSON.parse(response)
+
+    render :json => hash.dig('raw_payload', 'body'), :status => hash.dig('raw_payload', 'status')
   end
 
   private
 
-  def send_directive(directive, recipient, payload, socket_path = SOCKET_PATH)
-    socket = UNIXSocket.new(socket_path)
-    socket.send("#{recipient}\n#{directive}\n#{payload}" + DELIM, 0)
-    response = ''
-    loop do
-      received = socket.recv(1024)
-      done = received.include?(DELIM)
-      received.sub!(DELIM, '') if done
-
-      response += received
-      break if done
-    end
-    response
-  ensure
-    socket.close
+  def external_tenant
+    '1460290'
   end
 
-  def identity_headers(tenant)
-    {
-      "x-rh-identity" => Base64.strict_encode64(
-        JSON.dump({"identity" => {"account_number" => tenant}})
-      )
-    }
+  # This name has to be in sources db
+  def target_receptor
+    "node-a"
   end
 end
