9e9bcb1c0eea5c9a33a836e1dd37626006ea7b3b
Martin Slemr <mslemr@redhat.com>
2019-11-27 16:28:14 +0100

    Mutex synchronized requests

--- lib/receptor_proxy/server.rb
+++ lib/receptor_proxy/server.rb
@@ -1,9 +1,7 @@
 require "active_support/inflector"
 require "active_support/core_ext/string"
 require "socket"
-# require "surro-gate"
 require "json"
-require "pry-byebug"
 
 module ReceptorProxy
   class Server
@@ -11,65 +9,53 @@ module ReceptorProxy
     DELIM       = "\x1b[K".freeze
 
     def initialize
-      @source_uid = ENV['SOURCE_UID']
-      @endpoint = {
-        :scheme => ENV['ENDPOINT_SCHEME'] || 'https',
-        :host   => ENV['ENDPOINT_HOST'],
-        :receptor_node => ENV['ENDPOINT_RECEPTOR_NODE']
-      }
-      @auth = {
-        :username => ENV['AUTH_USERNAME'],
-        :password => ENV['AUTH_PASSWORD']
-      }
+      @mutex = Mutex.new
     end
 
     def call(env)
-      # Check source values
-      if @endpoint.values.select(&:blank?) ||
-         @auth.values.select(&:blank?)
-        return not_found
-      end
-
-      base_url = if @endpoint[:host] =~ %r{\Ahttps?:\/\/} # HACK: URI can't properly parse a URL with no scheme
-                   URI(File.join(@endpoint[:scheme], "://", @endpoint[:host]))
-                 else
-                   URI(@endpoint[:host])
-                 end
+      base_url = CGI.unescape(env['HTTP_X_RH_ENDPOINT'])
+      receptor_node_id = env['HTTP_X_RH_RECEPTOR']
 
       # Get all HTTP headers
       headers = {}
       env.select { |k, _v| k.start_with?('HTTP_') }.each do |key, val|
-        new_key          = key.sub(/^HTTP_/, '')
+        next if %w[HTTP_X_RH_RECEPTOR HTTP_X_RH_ENDPOINT].include?(key)
+
+        new_key = key.sub(/^HTTP_/, '').tr('_'.freeze, '-'.freeze)
         headers[new_key] = val
       end
 
-      headers['HOST'] = base_url
+      headers['HOST'] = base_url.to_s.gsub(/\Ahttps?:\/\//, '').split('/')[0]
 
-
+      # URI + String removes URI path if String starts with '/'
+      # This technique is used by Faraday client
       payload = {
         'method'  => env['REQUEST_METHOD'],
-        'url'     => File.join(base_url, env['REQUEST_URI']),
+        'url'     => URI(base_url.to_s) + env['REQUEST_URI'].to_s,
         'headers' => headers,
         'ssl'     => false
       }
 
       response = send_directive("receptor_http:execute",
-                                @endpoint[:receptor_node],
-                                payload.to_json)
-
+                                receptor_node_id,
+                                payload.to_json,
+                                SOCKET_PATH)
       hash = JSON.parse(response)
 
-      # resp                         = Rack::Response.new
-      # resp.headers['Content-Type'] = 'application/json'
-      # resp.body                    = [hash['raw_payload']['body'].to_json]
-      # resp.status                  = hash['raw_payload']['status']
+      resp                         = Rack::Response.new
+      resp.headers['Content-Type'] = 'application/json'
+
+      body = hash['raw_payload']
+      if body.kind_of?(Hash)
+        resp.body                    = [hash['raw_payload']['body']]
+        resp.status                  = hash['raw_payload']['status']
+      else
+        resp.body = [hash['raw_payload']]
+        resp.status = 502
+      end
 
       # Rack requires this line below
-      # TODO: Get HTTP Response Headers from receptor
-      return [hash['raw_payload']['status'],
-              {'Content-Type' => 'application/json'},
-              [hash['raw_payload']['body'].to_json]
-      ]
+      [resp.status, resp.headers, resp.body]
     rescue => ex
       puts ex.message
       puts ex.backtrace
@@ -80,31 +66,26 @@ module ReceptorProxy
 
     def send_directive(directive, recipient, payload, socket_path = SOCKET_PATH)
       socket = UNIXSocket.new(socket_path)
-      socket.send("#{recipient}\n#{directive}\n#{payload}" + DELIM, 0)
-      response = ''
-      loop do
-        received = socket.recv(1024)
-        done     = received.include?(DELIM)
-        received.sub!(DELIM, '') if done
 
-        response += received
-        break if done
+      @mutex.synchronize do
+        socket.send("#{recipient}\n#{directive}\n#{payload}" + DELIM, 0)
+        response = ''
+        loop do
+          received = socket.recv(1024)
+          done     = received.include?(DELIM)
+          received.sub!(DELIM, '') if done
+
+          response += received
+          break if done
+        end
+        response
       end
-      response
     ensure
       socket.close
     end
 
     def not_found
       [404, {'Content-Type' => 'text/plain'}, ['Not found!']]
-    end
-
-    def cleanup(*sockets)
-      # Omit `nil`s from the array
-      sockets.compact!
-      # Close the opened sockets and remove them from the proxy
-      sockets.each { |sock| sock.close unless sock.closed? }
-      @proxy.pop(*sockets) if sockets.length > 1
     end
   end
 end
